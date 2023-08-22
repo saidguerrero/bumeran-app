191e3ab0640e9d557e9f467d3073e51bfdb0da18
saidguerrero <guerrerosaid@gmail.com>
2023-08-22 12:11:24 -0600

    se agrega url API de railway

--- src/Config.js
+++ src/Config.js
@@ -2,13 +2,18 @@ const DEV_VARS = {
   URL_WS_TRAVEL_API: "http://localhost:9081/travelagency/api/v1",
 };
 
-const PROD_VARS = {
+const PROD_AWS_VARS = {
   URL_WS_TRAVEL_API:
     "http://api-rest-bumeran-aws-env.eba-ummp4ehp.us-east-2.elasticbeanstalk.com/travelagency/api/v1",
 };
 
+const PROD_RAILAPP_VARS = {
+  URL_WS_TRAVEL_API:
+    "https://racial-letter-production.up.railway.app//travelagency/api/v1",
+};
+
 export class Configs {
   constructor() {
-    this.current = PROD_VARS;
+    this.current = PROD_RAILAPP_VARS;
   }
 }
